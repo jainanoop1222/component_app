5cdbad2a9f300821016048dd23eccb8e5b59c4d5
Anoop Jain <anoopjain1222@gmail.com>
2017-10-31 17:41:16 +0530

    abc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Comp1Component } from './comp1/comp1.component';
+import { FooterComponent } from './footer/footer.component';
+import { LeftImageComponent } from './left-image/left-image.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Comp1Component,
+    FooterComponent,
+    LeftImageComponent
   ],
   imports: [
     BrowserModule
